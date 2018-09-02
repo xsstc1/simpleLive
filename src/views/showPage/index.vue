616f90c72f96fcf50ba57aed2f8030ff08a73310
xsstc1 <xsstc1@163.com>
2018-09-02 16:37:00 +0800

    common

--- src/views/showPage/index.vue
+++ src/views/showPage/index.vue
@@ -11,17 +11,17 @@
 
                 </header>
                 <div class="cb">
-                    <div class="w70 fl">
-                        <video-player  @ended="onPlayerEnded($event)" class="video-player-box" ref="videoPlayer" :options="playerOptions" :playsinline="true" customEventName="customstatechangedeventname">
+                    <div class="w70 fl" v-if="showOrNot">
+                        <video-player @ended="onPlayerEnded($event)" class="video-player-box" ref="videoPlayer" :options="playerOptions" :playsinline="true" customEventName="customstatechangedeventname">
                         </video-player>
                     </div>
                     <div class="fl header-left">
                         <el-row :gutter="10">
 
                             <el-col :span="12" v-for="data of headerImg" :key="data.id">
-                                <div class="pr oh cp" @click="showDIalog">
+                                <div class="pr oh cp header-img" @click="showDIalog">
                                     <div class="left-shadow"></div>
-                                    <img class="w " :src="data.img_url" alt="">
+                                    <img class="w" :src="data.img_url" alt="">
                                 </div>
                             </el-col>
 
@@ -34,8 +34,8 @@
         </div>
         <div class="container pb-50">
             <div>
-                <nav class="f20 mt-50 cb"><img class="nav-i fl mr-15" src="http://www.kzb.life/image/ts-star-2-1.svg">
-                    <span class="fl color-6"> 精选推荐</span>
+                <nav class="f20 mt-50 cb"><img class="nav-i fl mr-15" src="http://www.kzb.life/image/ts-star-2.svg">
+                    <span class="fl color-6">{{firstTitle}}</span>
                 </nav>
             </div>
             <div class="mt-50">
@@ -43,7 +43,7 @@
 
                     <el-col :span="4" v-for="data of recommendImg" :key="data.id">
                         <div class="pr bg-color-white radius5 oh cp" @click="showDIalog">
-                            <img class="w " :src="data.img_url" alt="">
+                            <div class="content-img"><img class="w " :src="data.img_url" alt=""></div>
                             <div class="p15">{{data.name}}</div>
                         </div>
                     </el-col>
@@ -55,14 +55,14 @@
         <div class="index-puple w">
             <div class="container pt-50 pb-50">
                 <div>
-                    <nav class="f20 cb mb-40"><img class="nav-i fl mr-15" src="http://www.kzb.life/image/ts-lightning.svg">
-                        <span class="fl color-6"> 近期最火</span>
+                    <nav class="f20 cb mb-40"><img class="nav-i fl mr-15" src="http://www.kzb.life/image/ts-lightning-2.svg">
+                        <span class="fl color-6">{{secondTitle}}</span>
                     </nav>
                 </div>
                 <el-row :gutter="10">
                     <el-col :span="4" v-for="data of hotImg" :key="data.id">
                         <div class="pr bg-color-white radius5 oh cp" @click="showDIalog">
-                            <img class="w " :src="data.img_url" alt="">
+                            <div class="content-img"><img class="w " :src="data.img_url" alt=""></div>
                             <div class="p15">{{data.name}}</div>
                         </div>
                     </el-col>
@@ -73,15 +73,15 @@
         </div>
         <div class="container pb-50">
             <div>
-                <nav class="f20 mt-50 cb"><img class="nav-i fl mr-15" src="http://www.kzb.life/image/ts-color-plate%20%281%29.svg">
-                    <span class="fl color-6"> 大家喜欢的</span>
+                <nav class="f20 mt-50 cb"><img class="nav-i fl mr-15" src="http://www.kzb.life/image/ts-color-plate.svg">
+                    <span class="fl color-6">{{thirdTitle}}</span>
                 </nav>
             </div>
             <div class="mt-50">
                 <el-row :gutter="10">
                     <el-col :span="4" v-for="data of likeImg" :key="data.id">
                         <div class="pr bg-color-white radius5 oh cp" @click="showDIalog">
-                            <img class="w " :src="data.img_url" alt="">
+                            <div class="content-img"><img class="w " :src="data.img_url" alt=""></div>
                             <div class="p15">{{data.name}}</div>
                         </div>
                     </el-col>
@@ -92,15 +92,15 @@
         </div>
         <div class="container pb-50">
             <div>
-                <nav class="f20 mt-50 cb"><img class="nav-i fl mr-15" src="http://www.kzb.life/image/ts-star-2-1.svg">
-                    <span class="fl color-6">值得关注的</span>
+                <nav class="f20 mt-50 cb"><img class="nav-i fl mr-15" src="http://www.kzb.life/image/ts-star-2.svg">
+                    <span class="fl color-6">{{forthTitle}}</span>
                 </nav>
             </div>
             <div class="mt-50">
                 <el-row :gutter="10">
                     <el-col :span="4" v-for="data of followImg" :key="data.id">
                         <div class="pr bg-color-white radius5 oh cp" @click="showDIalog">
-                            <img class="w " :src="data.img_url" alt="">
+                            <div class="content-img"><img class="w " :src="data.img_url" alt=""></div>
                             <div class="p15">{{data.name}}</div>
                         </div>
                     </el-col>
@@ -135,7 +135,7 @@
             </div>
 
         </footer>
-        <div class="fix-left">
+        <div class="fix-left" v-if="showOrNot">
             <div>
                 <a href="http://www.783882.com/PageRegister?Agent=evan123" target="_blank"><img class="w" src="http://www.kzb.life/image/bjl.png"></a>
             </div>
@@ -146,7 +146,7 @@
                 <a href="http://www.783882.com/PageRegister?Agent=evan123" target="_blank"><img class="w" src="http://www.kzb.life/image/bjl.png"></a>
             </div>
         </div>
-        <div class="fix-right">
+        <div class="fix-right" v-if="showOrNot">
             <div>
                 <a href="http://www.bg56777.com" target="_blank"><img class="w" src="http://www.kzb.life/image/bogou.png"></a>
             </div>
@@ -169,182 +169,211 @@
 </template>
 
 <script>
-import { videoPlayer } from 'vue-video-player'
-import 'video.js/dist/video-js.css'
-var data = []
-var now = new Date()
-var value = Math.random() * 1000
+import { videoPlayer } from "vue-video-player";
+import "video.js/dist/video-js.css";
+var data = [];
+var now = new Date();
+var value = Math.random() * 1000;
 export default {
-  data() {
-    return {
-      followImg: [],
-      headerImg: [],
-      recommendImg: [],
-      likeImg: [],
-      hotImg: [],
-      qrImg: '',
-      videoUrl: '',
-      dialogVisible: false,
-      intervalid: {},
-      intervalidPic: {},
-      page: 1,
-      total: 0,
-      PI: 3.1415926535897932384626,
-      ee: 0.00669342162296594323,
-      aData: 6378245.0,
-      activeIndex: 1,
-      dialogVisible: false,
-      projectData: [],
-      searchData: {
-        phone: '',
-        date: '',
-        password: ''
-      },
-      playerOptions: {
-        // videojs options
+    data() {
+        return {
+            showOrNot: true,
+            firstTitle: "",
+            secondTitle: "",
+            thirdTitle: "",
+            forthTitle: "",
+            followImg: [],
+            headerImg: [],
+            titleData: [],
+            recommendImg: [],
+            likeImg: [],
+            hotImg: [],
+            qrImg: "",
+            videoUrl: "",
+            dialogVisible: false,
+            intervalid: {},
+            intervalidPic: {},
+            page: 1,
+            total: 0,
+            PI: 3.1415926535897932384626,
+            ee: 0.00669342162296594323,
+            aData: 6378245.0,
+            activeIndex: 1,
+            dialogVisible: false,
+            projectData: [],
+            searchData: {
+                phone: "",
+                date: "",
+                password: ""
+            },
+            playerOptions: {
+                // videojs options
 
-        muted: false,
-        autoplay: true,
-        loop:true,
-        replay: true,
-        language: 'en',
-        playbackRates: [0.7, 1.0, 1.5, 2.0],
-        sources: [
-          {
-            type: 'video/mp4',
-            src: ''
-          }
-        ],
-        poster: '/static/images/author.jpg'
-      }
-    }
-  },
-  components: {
-    // "cd-header": header,
-    // "cd-footer": footer
-    videoPlayer
-  },
-  mounted() {
-    // this.getAll()
-    this.getQrImg()
-    this.getVideo()
-    this.getHeaderImg()
-    this.getRecommendImg()
-    this.getHotImg()
-    this.getlikeImg()
-    this.getFollowImg()
-  },
+                muted: false,
+                autoplay: true,
+                loop: true,
+                replay: true,
+                language: "en",
+                playbackRates: [0.7, 1.0, 1.5, 2.0],
+                sources: [
+                    {
+                        type: "video/mp4",
+                        src: ""
+                    }
+                ],
+                poster: "/static/images/author.jpg"
+            }
+        };
+    },
+    components: {
+        // "cd-header": header,
+        // "cd-footer": footer
+        videoPlayer
+    },
+    mounted() {
+        // this.getAll()
+        this.getQrImg();
+        this.getVideo();
+        this.getHeaderImg();
+        this.getRecommendImg();
+        this.getHotImg();
+        this.getlikeImg();
+        this.getFollowImg();
+        this.getTitle();
+    },
 
-  methods: {
-      onPlayerEnded(player){
-player
-      },
-      getFollowImg() {
-      this.publicFun.httpNode(
-        'get',
-        'live/getImg?classifyId=4',
-        null,
-        (status, data, message) => {
-          if (status == 200) {
-            this.followImg = data
-          } else {
-            this.$message.error(message)
-          }
+    methods: {
+        getTitle() {
+            this.publicFun.httpNode(
+                "get",
+                "live/getTitle",
+                null,
+                (status, data, message) => {
+                    if (status == 200) {
+                        this.titleData = data;
+                        data.forEach(item => {
+                            if (item.title_id == 1) {
+                                this.firstTitle = item.title_name;
+                            } else if (item.title_id == 2) {
+                                this.secondTitle = item.title_name;
+                            } else if (item.title_id == 3) {
+                                this.thirdTitle = item.title_name;
+                            } else if (item.title_id == 4) {
+                                this.forthTitle = item.title_name;
+                            }
+                        });
+                    } else {
+                        this.$message.error(message);
+                    }
+                }
+            );
+        },
+        getFollowImg() {
+            this.publicFun.httpNode(
+                "get",
+                "live/getImg?classifyId=4",
+                null,
+                (status, data, message) => {
+                    if (status == 200) {
+                        this.followImg = data;
+                    } else {
+                        this.$message.error(message);
+                    }
+                }
+            );
+        },
+        getlikeImg() {
+            this.publicFun.httpNode(
+                "get",
+                "live/getImg?classifyId=3",
+                null,
+                (status, data, message) => {
+                    if (status == 200) {
+                        this.likeImg = data;
+                    } else {
+                        this.$message.error(message);
+                    }
+                }
+            );
+        },
+        getHotImg() {
+            this.publicFun.httpNode(
+                "get",
+                "live/getImg?classifyId=2",
+                null,
+                (status, data, message) => {
+                    if (status == 200) {
+                        this.hotImg = data;
+                    } else {
+                        this.$message.error(message);
+                    }
+                }
+            );
+        },
+        getRecommendImg() {
+            this.publicFun.httpNode(
+                "get",
+                "live/getImg?classifyId=1",
+                null,
+                (status, data, message) => {
+                    if (status == 200) {
+                        this.recommendImg = data;
+                    } else {
+                        this.$message.error(message);
+                    }
+                }
+            );
+        },
+        getHeaderImg() {
+            this.publicFun.httpNode(
+                "get",
+                "live/getImg?classifyId=5",
+                null,
+                (status, data, message) => {
+                    if (status == 200) {
+                        this.headerImg = data;
+                    } else {
+                        this.$message.error(message);
+                    }
+                }
+            );
+        },
+        getVideo() {
+            this.publicFun.httpNode(
+                "get",
+                "live/getVideo",
+                null,
+                (status, data, message) => {
+                    if (status == 200) {
+                        this.playerOptions.sources[0].src = data[0].video_url;
+                    } else {
+                        this.$message.error(message);
+                    }
+                }
+            );
+        },
+        showDIalog() {
+            this.dialogVisible = true;
+        },
+        onRegister() {
+            this.$router.push({ path: "/register" });
+        },
+        getQrImg() {
+            this.publicFun.httpNode(
+                "get",
+                "qiniu/getQrImg",
+                null,
+                (status, data, message) => {
+                    if (status == 200) {
+                        this.qrImg = data[0].img_url;
+                    } else {
+                        this.$message.error(message);
+                    }
+                }
+            );
         }
-      )
-    },
-    getlikeImg() {
-      this.publicFun.httpNode(
-        'get',
-        'live/getImg?classifyId=3',
-        null,
-        (status, data, message) => {
-          if (status == 200) {
-            this.likeImg = data
-          } else {
-            this.$message.error(message)
-          }
-        }
-      )
-    },
-    getHotImg() {
-      this.publicFun.httpNode(
-        'get',
-        'live/getImg?classifyId=2',
-        null,
-        (status, data, message) => {
-          if (status == 200) {
-            this.hotImg = data
-          } else {
-            this.$message.error(message)
-          }
-        }
-      )
-    },
-    getRecommendImg() {
-      this.publicFun.httpNode(
-        'get',
-        'live/getImg?classifyId=1',
-        null,
-        (status, data, message) => {
-          if (status == 200) {
-            this.recommendImg = data
-          } else {
-            this.$message.error(message)
-          }
-        }
-      )
-    },
-    getHeaderImg() {
-      this.publicFun.httpNode(
-        'get',
-        'live/getImg?classifyId=5',
-        null,
-        (status, data, message) => {
-          if (status == 200) {
-            this.headerImg = data
-          } else {
-            this.$message.error(message)
-          }
-        }
-      )
-    },
-    getVideo() {
-      this.publicFun.httpNode(
-        'get',
-        'live/getVideo',
-        null,
-        (status, data, message) => {
-          if (status == 200) {
-            this.playerOptions.sources[0].src = data[0].video_url
-          } else {
-            this.$message.error(message)
-          }
-        }
-      )
-    },
-    showDIalog() {
-      this.dialogVisible = true
-    },
-    onRegister() {
-      this.$router.push({ path: '/register' })
-    },
-    getQrImg() {
-      this.publicFun.httpNode(
-        'get',
-        'qiniu/getQrImg',
-        null,
-        (status, data, message) => {
-          if (status == 200) {
-            this.qrImg = data[0].img_url
-          } else {
-            this.$message.error(message)
-          }
-        }
-      )
     }
-  }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -352,6 +381,14 @@ player
 body
     background-color #f1f1f1
     // background url('http://www.kzb.life/image/zb_bg.jpg')
+.header-img
+    width 142.5px
+    height 142.5px
+    overflow hidden
+.content-img
+    width 186.66px
+    height 186.66px
+    overflow hidden
 .el-col-4
     margin-top 20px
 .fix-left
